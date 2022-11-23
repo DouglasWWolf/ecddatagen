072e85aae57e5411f6e6438c2ef79b45eccb01cd
DouglasWWolf <douglaswolf@yahoo.com>
2022-11-22 19:04:45 -0800

    No change history kept

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 
 void execute();
 
+const uint32_t BYTES_PER_CYCLE = 32;
+const uint32_t CYCLES_PER_ROW  = 64;
+const uint32_t BYTES_PER_ROW   = BYTES_PER_CYCLE * CYCLES_PER_ROW;
 
 //=================================================================================================
 // This is the 32-byte wide structure of a single record.   A record contains 1 data-cycle's worth
@@ -60,7 +63,7 @@ void execute()
     for (int i=0; i<sizeof(record.filler); ++i) record.filler[i] = i;
 
     // How many rows will fit into 4GB?
-    uint32_t row_count = 0x100000000LL / 2048;
+    uint32_t row_count = 0x100000000LL / BYTES_PER_ROW;
 
     // Loop through every row of data we're going to write
     for (uint32_t row = 0; row < row_count; ++row)
@@ -69,14 +72,13 @@ void execute()
         record.row = row;
 
         // Loop through each of the 64 cycles in a row
-        for (int cycle = 0; cycle <64; ++cycle)
+        for (int cycle = 0; cycle <CYCLES_PER_ROW; ++cycle)
         {
             // Store the cycle number into the record
             record.cycle = cycle;
 
             // And write the record to disk
             fwrite(&record, 1, sizeof record, ofile);
-
         }
     }
 
